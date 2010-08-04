c102a6e567dc2005205cdcbaccb81710ab1b381d
Dawid Ciężarkiewicz <dpc@ucore.info>
2010-08-04 21:39:25 +0200

    Some work on screen shell wrapper.

--- .bashrc.screen.cmd.sh
+++ .bashrc.screen.cmd.sh
@@ -4,11 +4,16 @@ screen -xRR -S sshwrap
 
 # clean up after detach'ing
 # drop ssh identities
-ssh-add -D
+ssh-add -D 2>/dev/null
 
 # kill agent
 if [[ ! -z "$SSH_SHOULD_KILL_AGENT_ON_STOP" ]]; then
 	eval `ssh-agent -k`
 fi
+
+# set terminal title to something sane
+# just in case he does not restore
+# it itself
+echo -ne "\033]0;was: ${HOSTNAME%%.*}\007"
 
 exit 0
